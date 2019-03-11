91db5405189c5d1783b84d337f0bc40db6f86ca0
jaskaran <jaskaran.sarkaria@googlemail.com>
2019-03-11 20:14:13 +0000

    add addToOrder functionality

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -3,59 +3,6 @@ import OrderContainer from "../OrderContainer";
 import Menu from "../Menu";
 
 class App extends Component {
-<<<<<<< Updated upstream
-    constructor(props) {
-        super(props);
-        this.state = {
-            arrayOfBirds: [
-                {
-                    imageUrl: "",
-                    name: "Transylvania Naked Neck",
-                    age: "3 months",
-                    sex: "female",
-                    weight: "4kg",
-                    price: "65.99",
-                    origin: "Romania",
-                    stock: "2",
-                    id: "999" // use getshort id.
-                }
-            ],
-            order: [[999, 2, "Transylvania naked neck", "65.99"]]
-        };
-||||||| merged common ancestors
-  constructor(props) {
-    super(props);
-    this.state = {
-      arrayOfBirds: [
-        {
-          id: 999,
-          imageUrl: "",
-          name: "Transylvania Naked Neck",
-          age: "3 months",
-          sex: "female",
-          weight: "4kg",
-          price: "65.99",
-          origin: "Romania",
-          stock: "2"
-        }
-      ],
-      order: [[999, 2, "Transylvania naked neck", "65.99"]]
-    };
-  }
-
-  addToOrder = ids => {
-    if (!ids) return;
-    const birdList = this.state.arrayOfBirds;
-    let obj = birdList.find(objItem => {
-      return objItem.id === ids;
-    });
-    if (obj === undefined) {
-      return;
-    } else {
-      let orderArray = [obj, obj.stock, obj.name, obj.price];
-      this.setState({ order: [...this.state.order, orderArray] });
-      return;
-=======
   constructor(props) {
     super(props);
     this.state = {
@@ -87,26 +34,23 @@ class App extends Component {
       let orderArray = [obj, obj.stock, obj.name, obj.price];
       this.setState({ order: [...this.state.order, orderArray] });
       return;
->>>>>>> Stashed changes
     }
 
-    // addToOrder(){
-    //   return
-    //   /// get id of bird based on button click
-    //   // create array with [stock, price, name, id]
-    //   // setState({order})
-    //   // send this id and relevant data to order
-    // }
+    /// get id of bird based on button click
+    // create array with [stock, price, name, id]
+    // setState({order})
+    // send this id and relevant data to order
+  };
 
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header" />
-                <Menu />
-                <OrderContainer order={this.state.order} />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header" />
+        <Menu onClick={this.addToOrder} birds={this.state.arrayOfBirds} />
+        <OrderContainer order={this.state.order} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/MenuItem/index.js
+++ src/Components/MenuItem/index.js
@@ -3,27 +3,27 @@ import PropTypes from "prop-types";
 import style from "./MenuItem.module.css";
 
 const MenuItem = props => (
-    <li>
-        <img
-            className={style.imgThumb}
-            src={`${props.bird.imageUrl}`}
-            alt={`${props.bird.name}`}
-            style={{ float: "left" }}
-        />
-        <div>
-            <p>Name: {props.bird.name}</p>
-            <p>Age: {props.bird.age} months</p>
-            <p>Sex: {props.bird.sex}</p>
-            <p>Weight: {props.bird.weight}kg</p>
-            <p>Price: £{props.bird.price}</p>
-            <p>Country of Origin: {props.bird.origin}</p>
-            <button /*onClick={addToOrder}*/>Add to Order</button>
-        </div>
-    </li>
+  <li>
+    <img
+      className={style.imgThumb}
+      src={`${props.bird.imageUrl}`}
+      alt={`${props.bird.name}`}
+      style={{ float: "left" }}
+    />
+    <div>
+      <p>Name: {props.bird.name}</p>
+      <p>Age: {props.bird.age} months</p>
+      <p>Sex: {props.bird.sex}</p>
+      <p>Weight: {props.bird.weight}kg</p>
+      <p>Price: £{props.bird.price}</p>
+      <p>Country of Origin: {props.bird.origin}</p>
+      <button onClick={() => props.onClick(props.bird.id)}>Add to Order</button>
+    </div>
+  </li>
 );
 
 MenuItem.propTypes = {
-    bird: PropTypes.object
+  bird: PropTypes.object
 };
 
 export default MenuItem;
