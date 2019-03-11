709483332640b1f5e10edc37e256e58e38cf192a
Jonathan Lester <jonathanmarklester@gmail.com>
2019-03-11 14:59:40 +0000

    moved default props

--- src/Components/MenuItem/index.js
+++ src/Components/MenuItem/index.js
@@ -18,17 +18,4 @@ TodoList.propTypes = {
     bird: PropTypes.object
 };
 
-MenuItem.defaultProps = {
-    bird: {
-        imageUrl:
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Tyto_alba_-British_Wildlife_Centre%2C_Surrey%2C_England-8a_%281%29.jpg/330px-Tyto_alba_-British_Wildlife_Centre%2C_Surrey%2C_England-8a_%281%29.jpg",
-        name: "Barn Owl",
-        age: 36,
-        sex: "female",
-        weight: "0.52",
-        price: 500,
-        origin: "UK"
-    }
-};
-
 export default MenuItem;
