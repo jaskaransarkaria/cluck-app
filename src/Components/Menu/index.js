fc8c3b0fbf3a3719f1b90781ea398dbf32938608
Jonathan Lester <jonathanmarklester@gmail.com>
2019-03-11 15:01:07 +0000

    moved default props

--- src/Components/Menu/index.js
+++ src/Components/Menu/index.js
@@ -11,4 +11,17 @@ const Menu = props => (
     </ul>
 );
 
+MenuItem.defaultProps = {
+    [ {
+        imageUrl:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c6/Tyto_alba_-British_Wildlife_Centre%2C_Surrey%2C_England-8a_%281%29.jpg/330px-Tyto_alba_-British_Wildlife_Centre%2C_Surrey%2C_England-8a_%281%29.jpg",
+        name: "Barn Owl",
+        age: 36,
+        sex: "female",
+        weight: "0.52",
+        price: 500,
+        origin: "UK"
+    } ]
+};
+
 export default Menu;
